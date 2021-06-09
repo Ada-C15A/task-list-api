c76d9c9d18de6c057a46139fecc061c512c18f49
RETURNcaden <cadence1547@gmail.com>
2021-06-08 23:10:01 -0700

    add /task route

--- app/models/task.py
+++ app/models/task.py
@@ -4,3 +4,9 @@ from app import db
 
 class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text)
+    description = db.Column(db.Text)
+    completed_at = db.Column(db.DateTime,nullable=True)
+  
+
+    
--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,52 @@
-from flask import Blueprint
+# from app import db
+# from app.models.task import Task
+# from flask import Blueprint, request, make_response, jsonify
+
+# task_bp = Blueprint("tasks",__name__, url_prefix="/tasks")
+
+# @task_bp.route("",method=["POST"])
+# def handle_books():
+#     if request.method == "POST":
+#         request_body = request.json()
+#         new_task = Task(
+#                     title = request_body["title"],
+#                     description = request_body["description"],
+#                     complete_at = request_body["complete_at"] )
+        
+#         db.session.add(new_task)
+#         db.session.commit()
+
+#         return make_response(f"Task {new_task.title} successfully added")
+
+from app import db
+from app.models.task import Task
+from flask import request, Blueprint, make_response, jsonify
+
+tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+@tasks_bp.route("", methods=["GET", "POST"])
+def handle_tasks():
+    if request.method == "GET":
+        tasks = Task.query.all()
+        tasks_response = []
+        for task in tasks:
+            tasks_response.append({
+                "task_id": task.task_id,
+                "title": task.title,
+                "description": task.description,
+                "completed_at": task.completed_at
+              
+            })
+        return jsonify(tasks_response)
+    elif request.method == "POST": 
+        request_body = request.get_json()
+        new_task = Task(
+                    title = request_body["title"],
+                    description = request_body["description"],
+                    completed_at = request_body["completed_at"] )
+        db.session.add(new_task)
+        db.session.commit()
+
+        return make_response(f"Planet {new_task.name} successfully created", 201)
+
 
