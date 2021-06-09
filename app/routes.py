c7aa5957a0a26126966556b9231b5c4dfb1eef5b
denisseai <msdenisseanaya@gmail.com>
2021-06-09 14:54:38 -0700

    Completed setup, added dbs, added columns to dev db

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,62 @@
-from flask import Blueprint
+from app import db
+from app.models.task import Task
+from flask import request, Blueprint, make_response, jsonify
 
+tasks_bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks")
+
+@tasks_bp.route("", methods=["GET", "POST"])
+def handle_tasks():
+    if request.method == "GET":
+        title_from_url = request.args.get("title")
+        if title_from_url:
+            tasks = Task.query.filter_by(title=title_from_url)
+        else:
+            tasks = Task.query.all()
+        tasks_response = []
+        for task in tasks:
+            tasks_response.append({
+                "id": task.id,
+                "title": task.title,
+                "description": task.description,
+                "completed_at": task.completed_at
+            })
+        return jsonify(tasks_response)
+
+    elif request.method == "POST":
+        request_body = request.get_json()
+        new_task = Task(title=request_body["title"],
+                        description=request_body["description"],
+                        completed_at=request_body["completed_at"])
+
+        db.session.add(new_task)
+        db.session.commit()
+
+        return make_response(f"Task {new_task.title} successfully created", 201)
+
+@tasks_bp.route("/<tasks_id>", methods=["GET", "PUT", "DELETE"])
+def handle_planet(tasks_id):
+    task = Task.query.get(tasks_id)
+    if task is None:
+        return make_response("something went wrong", 404)
+
+    if request.method == "GET":
+        return {
+            "id": task.id,
+            "title": task.title,
+            "description": task.description,
+            "completed_at": task.completed_at
+        }
+    elif request.method == "PUT":
+        form_data = request.get_json()
+
+        task.title = form_data["title"]
+        task.description = form_data["description"]
+        task.completed_at = form_data["completed_at"]
+
+        db.session.commit()
+
+        return make_response(f"Task #{task.id} successfully updated!!")
+    elif request.method == "DELETE":
+        db.session.delete(task)
+        db.session.commit()
+        return make_response(f"Task #{task.id} successfully deleted")
