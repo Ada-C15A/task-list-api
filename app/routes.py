5668ec3d22affbfda73326c81ee60144f960de83
Naomi Quinones <naomiquinones@gmail.com>
2021-06-09 23:29:08 -0700

    Add task model, GET tasks route

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,19 @@
-from flask import Blueprint
+from flask import Blueprint, request, make_response, jsonify
+from app.models.task import Task
 
+tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+@tasks_bp.route("", methods=["GET"])
+def handle_tasks():
+    if request.method == "GET":
+        tasks = Task.query.all()
+        tasks_response = []
+        for task in tasks:
+            tasks_response.append({
+                "task_id": task.task_id,
+                "title": task.title,
+                "description": task.description,
+                "completed_at": task.completed_at
+            })
+        return jsonify(tasks_response)
+    
