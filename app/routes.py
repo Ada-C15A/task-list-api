cbb38d2cc39ab4067ca5ca1192fb45da4a0ce0ae
Andrea Valliere <amvalliere@gmail.com>
2021-06-13 18:12:08 -0700

    Adds goals blueprint; goals POST and GET; and GET, PUT, and DELETE goals by id. Passes wave 5 tests.

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,17 @@
 from app import db
 from app.models.task import Task
+from app.models.goal import Goal
 from flask import request, Blueprint, make_response, jsonify
 from sqlalchemy import desc
 from datetime import date
 import requests, os
 
 tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+goals_bp = Blueprint("goals", __name__, url_prefix="/goals")
 
+''''''''''''''''''''''''''''''''''''
+# # # # # # TASK ROUTES # # # # # #
+''''''''''''''''''''''''''''''''''''
 
 @tasks_bp.route("", methods=["POST", "GET"])
 def handle_tasks():
@@ -24,8 +29,7 @@ def handle_tasks():
         db.session.add(new_task)
         db.session.commit()
         db_task = {
-            "task":
-            {
+            "task": {
                 "id": new_task.task_id,
                 "title": new_task.title,
                 "description": new_task.description,
@@ -145,3 +149,77 @@ def handle_incomplete_task(task_id):
         }
     }
     return make_response(response, 200)
+
+''''''''''''''''''''''''''''''''''''
+# # # # # # GOAL ROUTES # # # # # #
+''''''''''''''''''''''''''''''''''''
+
+@goals_bp.route("", methods=["POST", "GET"])
+def handle_goals():
+    if request.method == "POST":
+        request_body = request.get_json()
+        title = request_body.get("title")
+
+        if not title:
+            response_body = {"details": "Invalid data"}
+            return make_response(response_body, 400)
+
+        new_goal = Goal(title=title)
+        db.session.add(new_goal)
+        db.session.commit()
+        db_goal = {
+            "goal": {
+                "id": new_goal.goal_id,
+                "title": new_goal.title
+            }
+        }
+        return db_goal, 201
+    elif request.method == "GET":
+        goals = Goal.query.all()
+
+        goals_response = []
+        for goal in goals:
+            goals_response.append({
+                "id": goal.goal_id,
+                "title": goal.title
+            })
+        goals_json_response = jsonify(goals_response)
+        return make_response(goals_json_response, 200)
+
+@goals_bp.route("/<goal_id>", methods=["GET", "PUT", "DELETE"])
+def handle_goal(goal_id):
+    goal = Goal.query.get(goal_id)
+    if goal is None:
+        return make_response("", 404)
+
+    if request.method == "GET":
+        selected_goal = {
+            "goal": {
+                "id": goal.goal_id,
+                "title": goal.title}
+        }
+        return make_response(selected_goal, 200)
+
+    elif request.method == "PUT":
+        request_body = request.get_json()
+
+        goal.title = request_body["title"]
+
+        updated_goal = {
+            "goal": {
+                "id": goal.goal_id,
+                "title": goal.title
+            }
+        }
+
+        db.session.commit()
+        return make_response(updated_goal, 200)
+
+    elif request.method == "DELETE":
+        response = {
+            "details":
+            f'Goal {goal.goal_id} "{goal.title}" successfully deleted'
+        }
+        db.session.delete(goal)
+        db.session.commit()
+        return make_response(response, 200)
