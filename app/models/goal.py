c7bdf78986d20d3a3c228d200f18d35a3ec76077
clear <clear>
2021-06-15 16:04:22 -0700

    worked on wave 6

--- app/models/goal.py
+++ app/models/goal.py
@@ -1,13 +1,14 @@
 from flask import current_app
 from app import db
+from app.models.task import Task
 
 class Goal(db.Model):
     goal_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
     tasks = db.relationship("Task", backref="task", lazy=True)
 
-    def goal_to_json(self):
-        return {
+    def goal_json(self):
+        return( {
             "id": self.goal_id,
             "title": self.title
-                }
+        }, 200)
